6d24935d43b654a8d5453aaa9fbf8933846188bb
webchemist <webchemist@gmail.com>
2014-01-21 23:19:54 +0300

    restructured, full and basic templates were separated

---
+++ base/common.mk
@@ -0,0 +1,36 @@
+.PHONY: all compile test clean
+
+REBAR_BIN = $(abspath ./)/rel/../rebar
+ifeq ($(wildcard $(REBAR_BIN)),)
+	REBAR_BIN := $(shell which rebar)
+endif
+REBAR = $(REBAR_BIN) -C rebar.config
+
+# MAKERS
+
+all: compile
+
+compile: update-deps
+	$(MAKE) pre-compile
+	$(REBAR) compile
+	$(MAKE) post-compile
+
+update-deps: deps/.updated
+deps/.updated:
+	$(REBAR) update-deps ignore_deps=true
+	@touch deps/.updated
+
+get-deps:
+	@$(REBAR) get-deps
+NAME = {{ name }}
+
+clean: pre-clean
+	$(REBAR) clean
+	rm -f rel/reltool.config
+	rm -rf rel/$(SERVICE_NAME)*
+	$(MAKE) post-clean
+
+# OVERWRITES
+pre-compile:
+post-compile:
+pre-clean:
